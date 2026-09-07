cfb90091afe824301f66289cb7f8e6f55cac58e6
Turia2 <mateucano@gmail.com>
2026-09-07 22:01:10 +0200

    feat initial state and superposition in Cayley graph class

--- include/Cayley.hpp
+++ include/Cayley.hpp
@@ -5,7 +5,6 @@
 
 using Scalar = std::complex<double>;
 using State = Eigen::VectorX<Scalar>;
-using SpMat = Eigen::SparseMatrix<Scalar>;
 
 class Cayley{
     public:
@@ -15,6 +14,24 @@ class Cayley{
 
         int indx(int x, int s) const{ 
             return 2*((x % N + N) + s); 
+        }
+
+        State ini_state(int x0, int spin) const {
+
+            State psi = State::Zero(2*N);
+            psi(indx(x0, spin)) = Scalar(1.0, 0.0);
+            return psi;
 
         }
+
+        State ini_superp(int x0) const{
+
+            static const Scalar inv2 = 1.0/std::sqrt(2.0);
+            State psi = State::Zero(2*N);
+            psi(indx(x0,0)) = inv2;
+            psi(indx(x0, 1)) = Scalar(0.0, 1.0)*inv2;
+            return psi;
+
+        }
+
 };
